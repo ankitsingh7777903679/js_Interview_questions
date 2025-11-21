294d4c1ce14adca031aadffd06e24d342a71d9ca
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-11-21 15:14:33 +0530

    Q4

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // Q3 use console.log to display the current year
 
-let currentYear = new Date();
-console.log(currentYear.getFullYear())
+// let currentYear = new Date();
+// console.log(currentYear.getFullYear())
+
+// Q4 Create a two variable for farst name and lant name, concate and log them.
+let firstName = "ankit";
+let lastName = "singh"
+console.log(`${firstName} ${lastName}`)
