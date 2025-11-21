afa4e057a08905b15909886a6a1e344b1f517eba
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-11-21 15:17:17 +0530

    Q5

--- script.js
+++ script.js
@@ -4,6 +4,13 @@
 // console.log(currentYear.getFullYear())
 
 // Q4 Create a two variable for farst name and lant name, concate and log them.
-let firstName = "ankit";
-let lastName = "singh"
-console.log(`${firstName} ${lastName}`)
+// let firstName = "ankit";
+// let lastName = "singh"
+// console.log(`${firstName} ${lastName}`)
+
+
+// Q5 Track the value of variable by logging it before and after updating
+let val = 34
+console.log(val)
+val = 45
+console.log(val)
