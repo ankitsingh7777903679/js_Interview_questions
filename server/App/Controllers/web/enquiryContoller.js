01f1f4ce75bbfbc7e673f8cfddb90a9cbb752b26
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-12-13 14:49:15 +0530

    feat: implement CRUD operations for enquiries with delete and update functionality

--- server/App/Controllers/web/enquiryContoller.js
+++ server/App/Controllers/web/enquiryContoller.js
@@ -24,4 +24,38 @@ let enquiryList =async (req, res) =>{
     })
 }
 
-module.exports = {enquiryInsert, enquiryList}
+let enquiryDelete = async (req, res) => {
+    let id = req.params.id;
+    await enquiryModel.findByIdAndDelete(id).then(()=>{
+        res.send({status:1, message:"Enquiry deleted successfully"})
+    })
+
+}
+let enquirySingleRow = async(req, res) =>{
+    let id = req.params.id;
+    await enquiryModel.findOne({_id: id}).then((data)=>{
+        console.log(data);
+        res.send({status:1, data:data})
+    }).catch((err)=>{
+        res.send({status:0, message:err})
+    })
+}
+
+let enquiryUpdate = async(req, res) =>{
+    let id = req.params.id;
+    let {name, email, phone, message} = req.body;
+    let updateObj = {
+        name,
+        email,
+        phone,
+        message,
+    }
+    await enquiryModel.updateOne({_id: id}, updateObj).then((data)=>{
+        console.log(data);
+        res.send({status:1, data:data})
+    }).catch((err)=>{
+        res.send({status:0, message:err})
+    })
+}
+
+module.exports = {enquiryInsert, enquiryList, enquiryDelete, enquirySingleRow, enquiryUpdate};
--- server/App/routes/web/enquiryRoutes.js
+++ server/App/routes/web/enquiryRoutes.js
@@ -1,8 +1,11 @@
 let express = require('express');
-const { enquiryInsert, enquiryList } = require('../../Controllers/web/enquiryContoller');
+const { enquiryInsert, enquiryList, enquiryDelete, enquirySingleRow, enquiryUpdate } = require('../../Controllers/web/enquiryContoller');
 let enquiryRouter = express.Router();
 
 enquiryRouter.post('/insert', enquiryInsert)
 enquiryRouter.get('/list', enquiryList)
+enquiryRouter.delete('/delete/:id', enquiryDelete)
+enquiryRouter.get('/singleRow/:id', enquirySingleRow)
+enquiryRouter.put('/update/:id', enquiryUpdate);
 
 module.exports = enquiryRouter;
