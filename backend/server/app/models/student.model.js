c01e008dcd07d470bb61847540dd979bcfeef1b5
Ankit singh <149090246+ankitsingh7777903679@users.noreply.github.com>
2025-12-15 11:35:48 +0530

    feat: create student model schema for managing student data

---
+++ backend/server/app/models/student.model.js
@@ -0,0 +1,26 @@
+// use for making student model schema and export it to use in other files
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+
+let studentSchema = new Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    rollno:{
+        type:String,
+        required: true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    phone:{
+        type:String,
+        required:true
+    }
+})
+
+let studentModel = mongoose.model('student', studentSchema)
+module.exports = {studentModel}
